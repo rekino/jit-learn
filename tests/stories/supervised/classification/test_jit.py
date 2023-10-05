ec0ee3051157a6ed0ff4b880b2117b4ee28e0dc3
Ramin Barati <ramin.barati@carnext.com>
2023-10-05 15:33:41 +0330

    change test naming

--- tests/stories/supervised/classification/test_jit.py
+++ tests/stories/supervised/classification/test_jit.py
@@ -14,7 +14,7 @@ class TestInterface(unittest.TestCase):
         self.X_train = np.linspace(-1, 1)
         self.y_train = np.sign(self.X_train)
 
-    def test_supervised_base_classifier(self) -> None:
+    def test_base(self) -> None:
         learner = jit.BaseClassifier()
 
         learner.fit(self.X_train, self.y_train)
