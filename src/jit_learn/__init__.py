25b9931af2e2772133c38a2b46f2243a34f07ed7
Ramin Barati <ramin.barati@carnext.com>
2023-10-06 20:39:39 +0330

    fix the cvx problem to conform to DPP rules

---
+++ src/jit_learn/__init__.py
@@ -0,0 +1,1 @@
+from .supervised.classification.base import BaseClassifier
--- src/jit_learn/supervised/classification/base.py
+++ src/jit_learn/supervised/classification/base.py
@@ -1,3 +1,4 @@
+import numpy as np
 import cvxpy as cp
 from numpy.typing import NDArray
 from typing import Sequence
@@ -7,13 +8,18 @@ class BaseClassifier(object):
     def __init__(self, xdim: int, memory: int) -> None:
         w = self.weights = cp.Variable(xdim)
         e = self.errors = cp.Variable(memory)
+        p = self.preds = cp.Variable(memory)
         b = self.bias = cp.Variable()
 
         X = self.features = cp.Parameter((memory, xdim))
         y = self.labels = cp.Parameter(memory)
         C = self.param = cp.Parameter()
 
-        constraints = [cp.multiply(y, X @ w + b) >= 1 - e, e >= 0]
+        constraints = [
+            cp.multiply(y, p) >= 1 - e,
+            p == X @ w + b,
+            e >= 0
+            ]
 
         objective = cp.Minimize(cp.sum_squares(w) / 2 + C * cp.sum(e))
 
@@ -47,7 +53,7 @@ class BaseClassifier(object):
         lower_bound = (1 - ep - Xp @ w).max()
         upper_bound = (en - 1 - Xn @ w).min()
 
-        return lower_bound, upper_bound, w, e
+        return np.asarray([lower_bound, upper_bound])
 
     def fit(self, X: NDArray, y: NDArray) -> None:
         self.X = X
@@ -58,6 +64,6 @@ class BaseClassifier(object):
         transformed = self._transform(x, samples)
         features = self._embed(transformed)
 
-        bound, _, _ = self._train(features, labels)
+        bound = self._train(features, labels)
 
         return bound
