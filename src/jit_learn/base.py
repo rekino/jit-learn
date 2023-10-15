24756c866365abee66e937618be164e3d9adc581
ba <ba@rog.com>
2023-10-15 16:00:46 +0330

    add kwargs to interfaces

--- src/jit_learn/base.py
+++ src/jit_learn/base.py
@@ -1,27 +1,32 @@
+from typing import Any
 from numpy.typing import NDArray
 
 
 class Distribution:
-    def fit(self, data: NDArray, **kwargs) -> None:
+    def fit(self, data: NDArray, **kwargs: Any) -> None:
         raise NotImplementedError()
 
-    def sample(self, size: int, **kwargs) -> NDArray:
+    def sample(self, size: int, **kwargs: Any) -> NDArray:
         raise NotImplementedError()
 
 
 class Automorphism:
-    def transform(self, x: NDArray, X: NDArray, **kwargs) -> NDArray:
+    def transform(self, x: NDArray, X: NDArray, **kwargs: Any) -> NDArray:
         raise NotImplementedError()
 
 
 class Embeding:
-    def embed(self, X: NDArray, **kwargs) -> NDArray:
+    def embed(self, X: NDArray, **kwargs: Any) -> NDArray:
         raise NotImplementedError()
 
 
 class Model:
-    def train(self, X: NDArray, y: NDArray, **kwargs) -> None:
+    def train(self, X: NDArray, y: NDArray, **kwargs: Any) -> None:
         raise NotImplementedError()
 
-    def predict(self, x: NDArray, **kwargs) -> NDArray:
+    def __call__(self, x: NDArray, **kwargs: Any) -> NDArray:
         raise NotImplementedError()
+
+    @property
+    def is_ready(self):
+        return False
