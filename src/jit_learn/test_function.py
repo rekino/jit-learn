64c95e98f3ecff505c9b6919e952c146bab6284b
Ramin Barati <ramin.barati@carnext.com>
2024-07-20 03:03:12 +0330

    add SchwartzNeuron

---
+++ src/jit_learn/test_function.py
@@ -0,0 +1,36 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SchwartzNeuron(nn.Module):
+    def __init__(self, dim: int, activation=F.tanh) -> None:
+        super().__init__()
+
+        self.dim = dim
+        self.w = nn.Parameter(torch.randn(dim, 1))
+        self.b = nn.Parameter(torch.randn(1))
+        self.activation = activation
+
+        self.norm = np.exp(-dim * np.log(2 * np.pi) / 2)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        out = (x*x).sum(dim=-1, keepdim=True)
+        out = torch.exp(-out/2) * self.norm
+        out *= self.activation(x @ self.w + self.b)
+        return out
+
+    def laplacian(self, x: torch.Tensor) -> torch.Tensor:
+        w_w = self.w.T @ self.w
+        w_x = x @ self.w
+        x_x = (x*x).sum(dim=-1, keepdim=True)
+
+        s = self.activation(x @ self.w + self.b)
+        s_x = 1 - s**2
+        s_xx = 2 * (s**3 - s)
+
+        out = s_xx * w_w - 2 * s_x * w_x + s * (x_x - self.dim)
+        out *= torch.exp(-x_x/2) * self.norm
+
+        return out
---
+++ tests/test_schwartz.py
@@ -0,0 +1,28 @@
+import unittest
+
+import torch
+
+from src.jit_learn.test_function import SchwartzNeuron
+
+
+class TestSchwartzNeuron(unittest.TestCase):
+    def test_init(self) -> None:
+        rho = SchwartzNeuron(2)
+
+        self.assertIsNotNone(rho)
+    
+    def test_forward(self) -> None:
+        rho = SchwartzNeuron(2)
+
+        x = torch.randn(3, 2)
+        out = rho(x)
+
+        self.assertEquals(out.shape, (3, 1))
+    
+    def test_laplacian(self) -> None:
+        rho = SchwartzNeuron(2)
+
+        x = torch.randn(3, 2)
+        out = rho.laplacian(x)
+
+        self.assertEquals(out.shape, (3, 1))
